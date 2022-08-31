b4e7c026270292143d3f7e576dae3e7fe14c5119
Cegoch <cesar.gonzalo@pucp.edu.pe>
2022-08-31 18:53:25 -0500

    actualizado

--- src/pe/edu/pucp/jalamep/gesttransaccion/model/MetodoTransaccion.java
+++ src/pe/edu/pucp/jalamep/gesttransaccion/model/MetodoTransaccion.java
@@ -3,5 +3,29 @@ package pe.edu.pucp.jalamep.gesttransaccion.model;
 public class MetodoTransaccion {
     private String tipo;
     private String moneda;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMoneda() {
+        return moneda;
+    }
+
+    public void setMoneda(String moneda) {
+        this.moneda = moneda;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
     private boolean activo;
 }
