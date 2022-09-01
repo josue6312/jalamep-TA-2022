2bfc9f9e422a50f8f703dcda30645b74d66aeddd
josue6312 <83036196+josue6312@users.noreply.github.com>
2022-08-31 19:05:20 -0500

    hola

---
+++ src/pe/edu/pucp/jalamep/rrhh/model/Pasajero.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.jalamep.rrhh.model;
+
+/**
+ *
+ * @author Asus
+ */
+public class Pasajero extends Persona{
+   /* private Destino preferencial;*/
+    
+}
